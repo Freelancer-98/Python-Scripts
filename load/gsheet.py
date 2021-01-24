a355776de2f0e1d4fbf25fd18754e5b7b2bb622a
nilshah98 <neel12@somaiya.edu>
2021-01-24 14:28:59 +0530

    [CONFIG] (gsheet) Changed credentials path

--- load/gsheet.py
+++ load/gsheet.py
@@ -5,16 +5,16 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-# If modifying these scopes, delete the file token.pickle.
+# If modifying these scopes, delete the file gsheet_token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 def get_authenticated_service():
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
+    # The file gsheet_token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists('env/gsheet_token.pickle'):
+        with open('env/gsheet_token.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -22,10 +22,10 @@ def get_authenticated_service():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'env/gsheets/credentials.json', SCOPES)
+                'env/gsheet_credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open('env/gsheet_token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('sheets', 'v4', credentials=creds)
