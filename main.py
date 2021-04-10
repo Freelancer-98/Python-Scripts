648e55346c8c45ade018b472347e57574cca2417
nilshah98 <neel12@somaiya.edu>
2021-04-10 16:29:46 +0530

    [FEAT] (*) Revamp complete arch

--- main.py
+++ main.py
@@ -1,47 +1,10 @@
 from env.pocket_credentials import consumer_key, access_token
-from env.gsheet_config import pocket_spreadsheet, pocket_range
-from extract.pocket import extract_pocket, extract_gsheet
-from transform.pocket import get_new_items, get_past_x_days_stats, get_random_older_article
-from load.gsheet import Gsheet
-from datetime import datetime
-from helpers import create_pocket_template, send_pocket_notification
-import argparse
+from src.pocket import extract_pocket
 
-# Workflows
-def update_pocket(consumer_key, access_token, pocket_spreadsheet, pocket_range):
-    print(f"Extracting pocket articles via pockexport... ⏳")
+def main():
     extract_pocket(consumer_key, access_token)
-    print(f"Extraction complete... ✔️")
-    print(f"Updating GSheet... ⏳")
-    pocket_new_data = get_new_items()
-    pocket_spreadsheet = Gsheet(pocket_spreadsheet, pocket_range)
-    pocket_spreadsheet.clear()
-    pocket_spreadsheet.update('USER_ENTERED', {'values': pocket_new_data})
-    print(f"Updation complete... ✔️")
-
-def send_weekly_stat(d):
-    print(f"Sending weekly stats... ⏳")
-    weekly_stats = get_past_x_days_stats(d)
-    weekly_template = create_pocket_template(weekly_stats)
-    send_pocket_notification(f"{datetime.today().strftime('%d-%m-%Y')} ending week pocket stats", weekly_template)
-    print(f"Sent weekly stats... ✔️")
-
-def send_random(d):
-    print(f"Sending weekly random article... ⏳")
-    random_article = get_random_older_article(d)
-    random_template = create_pocket_template(random_article)
-    send_pocket_notification(f"{datetime.today().strftime('%d-%m-%Y')} ending week random article", random_template)
-    print(f"Sent weekly random article... ✔️")
-
-def main(d):
-    update_pocket(consumer_key, access_token, pocket_spreadsheet, pocket_range)
-    send_weekly_stat(d)
-    send_random(d)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-pd", "--pocketDays", type=int, default=7, help="Fetch pocket records of past X days")
-    args = parser.parse_args()
-    main(args.pocketDays)
+    main()
 
 
---
+++ src/pocket.py
@@ -0,0 +1,38 @@
+import pockexport.export
+import json
+from datetime import datetime
+
+def extract_pocket(consumer_key, access_token):
+    # 01. Extracting everything
+    pocket_data = pockexport.export.get_json(consumer_key=consumer_key, access_token=access_token)    
+
+    # 02. Cleaning data
+    # Structure => { ID: { Date Read, Title, Author, Link, Tags, Annotations, Favorite, Source }, ... } 
+    pocket_articles = pocket_data['list']
+    pocket_clean = {}
+
+    for id in pocket_articles.keys():
+        article = pocket_articles[id]
+        if article['time_read'] != "0" and len(article['resolved_title']) > 0:
+            
+            # 01. Uniformize the data
+            if 'authors' not in article.keys(): article['authors'] = {}
+            if 'tags' not in article.keys(): article['tags'] = {}
+            if 'annotations' not in article.keys(): article['annotations'] = []
+
+            # 02. Getting the required data
+            time_read = datetime.fromtimestamp(int(article['time_read'])).strftime('%Y-%m-%d')
+            title = article['resolved_title']
+            author = list(map(lambda x: article['authors'][x]['name'], article['authors'].keys()))
+            link = article['resolved_url']
+            tags = list(article['tags'].keys())
+            annotations = list(map(lambda annotation: annotation['quote'], article['annotations']))
+            favorite = int(article['favorite'])
+            source = 'pocket'
+
+            # 03. Appending to dictionary
+            pocket_clean[id] = {'time_read': time_read, 'title': title, 'author': author, 'link': link, 
+                                'tags': tags, 'annotations': annotations, 'favorite': favorite, 'source': source}
+    
+    with open('data/pocket.json', 'w+') as outfile:
+        json.dump(pocket_clean,outfile,indent=2)
