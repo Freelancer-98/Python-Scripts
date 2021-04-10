434329e04ee90547eed62510b3b3fa470d8ef842
nilshah98 <neel12@somaiya.edu>
2021-04-10 17:42:37 +0530

    [FEAT] (main.py) Consume key using environment vars

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from env.pocket_credentials import consumer_key, access_token
+import os
 from src.pocket import extract_pocket
 
+POCKET_CONSUMER_KEY = os.environ.get('POCKET_CONSUMER_KEY')
+POCKET_ACCESS_TOKEN = os.environ.get('POCKET_ACCESS_TOKEN')
+
 def main():
-    extract_pocket(consumer_key, access_token)
+    extract_pocket(POCKET_CONSUMER_KEY, POCKET_ACCESS_TOKEN)
 
 if __name__ == '__main__':
     main()
