fafda1d095e3d4e8b83f2c26fa381a5050a641b9
nilshah98 <neel12@somaiya.edu>
2021-04-16 15:55:52 +0530

    [DOCS] Added content for index page

--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <h1>Index Page</h1>
+    <div>
+        <h1>Welcome to my reads page</h1>
+        <p>Currently showcasing my pocket scraped which occur once per day</p>
+        <p>Checkout - <a href="https://github.com/nilshah98/Knowledge-Lake">source code</a></p>
+        <p>Checkout - <a href="https://read.nilshah98.me/read">pocket scrapes</a></p>
+    </div>
   )
 }
